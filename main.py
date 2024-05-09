b588cb535de096667e07bd855b8a43fe83e227ec
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-05-09 18:48:06 +0700

    Number validations: floats, greater than and less than

--- main.py
+++ main.py
@@ -60,3 +60,30 @@ async def read_item(
 
   return response
 """
+
+# Number validations: floats, greater than and less than
+@app.get("/items/{id}")
+async def read_item(
+  id: Annotated[
+    int,
+    Path(
+      gt = 0,
+      le = 1_000,
+      description = "The ID of the item to get"
+    )
+  ],
+  q: Annotated[str, Query()],
+  size: Annotated[
+    float,
+    Query(
+      gt = 0,
+      lt = 10.5
+    )
+  ]
+):
+  response = {"id": id}
+
+  if q:
+    response.update({"q": q})
+
+  return response
